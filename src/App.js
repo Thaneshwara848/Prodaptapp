8077a0ba9c073c2d8eeaf4383af589c7c1fb4700
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2025-08-07 11:26:20 +0530

    Demo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Thanesh
+         Thanesh Demo
         </p>
         <a
           className="App-link"
